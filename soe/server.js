63d2aedacfc590e3db1b6b0f18c1516063f21458
Max Stuart <maxastuart@gmail.com>
2020-02-29 04:16:50 -0800

    got watson responding - almost getting text-to-speech!

--- soe/server.js
+++ soe/server.js
@@ -1,63 +1,79 @@
 'use strict';
-// load environment properties from a .env file for local development
 require('dotenv').load();
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const expressWs = require('express-ws')(app);
-
 const Nexmo = require('nexmo');
 const { Readable } = require('stream');
-
-const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
+const gSpeech = require('@google-cloud/speech');
+const AssistantV2 = require('ibm-watson/assistant/v2');
 const { IamAuthenticator } = require('ibm-watson/auth');
-
-const speechToText = new SpeechToTextV1({
+const gSTTclient = new gSpeech.SpeechClient(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const assistant = new AssistantV2({
+  version: '2020-02-05',
   authenticator: new IamAuthenticator({
-    apikey: process.env.SPEECH_TO_TEXT_IAM_APIKEY,
-  }),
-  url: process.env.SPEECH_TO_TEXT_URL,
+    apikey: process.env.ASSISTANT_APIKEY
+  })
 });
-
-// const speech = require('@google-cloud/speech');
-// // use GOOGLE_APPLICATION_CREDENTIALS to point to the info google-cloud/speech needs
-// const client = new speech.SpeechClient(null);
-
 const nexmo = new Nexmo({
-  apiKey: "dummy",
-  apiSecret: "dummy",
-  applicationId: process.env.APP_ID,
+  apiKey: process.env.NEXMO_API_KEY,
+  apiSecret: process.env.NEXMO_API_SECRET,
+  applicationId: process.env.NEXMO_APP_ID,
   privateKey: process.env.PRIVATE_KEY || './private.key'
-});
+}, { debug: true });
 
 app.use(bodyParser.json());
 
 app.get('/ncco', (req, res) => {
-
-  let nccoResponse = [
-    {
-      "action": "connect",
-      "endpoint": [{
-        "type": "websocket",
-        "content-type": "audio/l16;rate=16000",
-        "uri": `ws://${req.hostname}/socket`
-      }]
-    }
-  ];
-
+  let nccoResponse = [{
+    "action": "connect",
+    "endpoint": [{
+      "type": "websocket",
+      "content-type": "audio/l16;rate=16000",
+      "uri": `ws://${req.hostname}/socket`
+    }]
+  }];
   res.status(200).json(nccoResponse);
 });
 
+let caller = null;
+let callUUID = null;
 app.post('/event', (req, res) => {
-  console.log('EVENT LOG::', req.body);
+  if (req.body.from !== 'Unknown') {
+    caller = req.body.from;
+    callUUID = req.body.conversation_uuid.slice(5);
+  }
+  console.log('EVENT from', caller, 'to', req.body.to, req.body.status);
+  // console.log('EVENT LOG::', req.body);
   res.status(204).end();
 });
 
 // Nexmo Websocket Handler
 app.ws('/socket', (ws, req) => {
 
-  let request = {
+  let wSessionID = null;
+
+  assistant.createSession({
+    assistantId: process.env.WATSON_ASSISTANT_ID
+  }).then(res => {
+    wSessionID = res.result.session_id;
+    // get watson to play welcome message to caller
+    assistant.message({
+      assistantId: process.env.WATSON_ASSISTANT_ID,
+      sessionId: wSessionID,
+      input: { 'text': 'Hello' }
+    }).then(res => {
+      // console.log(JSON.stringify(res, null, 2));
+      console.log('Darcel:', res.result.output.generic[0].text);
+      nexmo.calls.talk.start(callUUID, {
+        text: res.result.output.generic[0].text
+      }).then(res => { console.log(res); }).catch(err => { console.log(err); });
+    }).catch(err => { console.log(err); });
+  }).catch(err => { console.log(err); });
+
+  let gSTTparams = {
     config: {
       encoding: 'LINEAR16',
       sampleRateHertz: 16000,
@@ -66,24 +82,24 @@ app.ws('/socket', (ws, req) => {
     interimResults: false
   };
 
-  // const recognizeStream = client
-  //   .streamingRecognize(request)
-  //   .on('error', console.error)
-  //   .on('data', data => {
-  //     console.log(
-  //       `Transcription: ${data.results[0].alternatives[0].transcript}`
-  //     );
-  //   });
-
-  var params = {
-    objectMode: true,
-    contentType: 'audio/l16;rate=16000',
-    model: 'en-US_BroadbandModel'
-  };
-
-  const recognizeStream = speechToText.recognizeUsingWebSocket(params);
-  recognizeStream.on('data', data => console.log(`Transcription: ${data.results[0].alternatives[0].transcript}`));
-  recognizeStream.on('error', console.error);
+  const recognizeStream = gSTTclient
+    .streamingRecognize(gSTTparams)
+    .on('error', console.error)
+    .on('data', data => {
+      console.log(`${caller}: ${data.results[0].alternatives[0].transcript}`);
+      // and send to watson assistant
+      assistant.message({
+        assistantId: process.env.WATSON_ASSISTANT_ID,
+        sessionId: wSessionID,
+        input: { 'text': data.results[0].alternatives[0].transcript }
+      }).then(res => {
+        // console.log(JSON.stringify(res, null, 2));
+        console.log('Darcel:', res.result.output.generic[0].text);
+        nexmo.calls.talk.start(callUUID, {
+          text: res.result.output.generic[0].text
+        }).then(res => { console.log(res); }).catch(err => { console.log(err); });
+      }).catch(err => { console.log(err); });
+    });
 
   ws.on('message', (msg) => {
     if (typeof msg === "string") {
@@ -94,6 +110,7 @@ app.ws('/socket', (ws, req) => {
   });
 
   ws.on('close', () => {
+    console.log('CALLER HUNG UP');
     recognizeStream.destroy();
   });
 });
